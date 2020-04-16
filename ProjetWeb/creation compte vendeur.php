6256012b809e7dd58d787ec60d74d3f2f328ad19
GuezMrt <61501717+GuezMrt@users.noreply.github.com>
2020-04-16 14:45:03 +0200

    Add files via upload

---
+++ ProjetWeb/creation compte vendeur.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html> 
+<html> 
+<head>  
+	<title>création compte vendeur</title>  
+	<meta charset="utf-8"> 
+</head> 
+<body>  
+	<h2>Ajout d'un vendeur</h2>  
+	<form action="ajouter vendeur.php" method="post">   
+		<table>    
+			<tr>
+				<td>Pseudo:</td>     
+				<td><input type="text" name="Pseudo"></td>    
+			</tr>    
+			<tr>     
+				<td>Email:</td>     
+				<td><input type="text" name="Email"></td>    
+			</tr>
+			<tr>     
+				<td>Nom:</td>     
+				<td><input type="text" name="Nom"></td>    
+			</tr>       
+			<tr>     
+				<td>Photo_p:</td>     
+				<td><input type="text" name="Photo_p"></td>    
+			</tr>
+			<tr>     
+				<td>Image_pref:</td>     
+				<td><input type="text" name="Image_pref"></td>    
+			</tr>       
+			<tr>     
+				<td colspan="2" align="center">                                
+					<input type="submit" name="button2" value="Ajouter"></td>    
+				</tr>   
+			</table>  
+		</form> 
+	</body> 
+</html> 
