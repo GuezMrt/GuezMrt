71fe0a0426e07f4cfee608b16732e2f4d5bc1afe
gauthierbcn <49686586+gauthierbcn@users.noreply.github.com>
2020-04-19 19:14:29 +0200

    Update connexion_vendeur.php

--- ProjetWeb/connexion_vendeur.php
+++ ProjetWeb/connexion_vendeur.php
@@ -6,13 +6,78 @@ session_start();
 <head>
 <meta charset="utf-8" />
 <title>Connection d'un vendeur</title>
+ 
+            <title>EbayECE</title>
+        
+        <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script type="text/javascript"></script>
+        <link href="http://fr.allfont.net/allfont.css?fonts=futura-normal" rel="stylesheet" type="text/css"/>
+      
+        <style>
+            
+        body 
+        {
+          
+            font-family: 'Futura-Normal', arial;
+           
+        
+        }
+        </style>
+        
+ 
 	
 </head>
 <body>
-<p>Bonjour</p>
-<p>Bienvenue sur notre site.</p>
-<div class="content">
+<!-- Navbar -->
     <form action="connexion_vendeur.php" method="post">
+        <nav class="navbar navbar-expand-sm fixed-top">
+     
+            <div class="searchbar fixed-top">
+            <input class="search_input" type="text" name="" placeholder="Rechercher...">
+                <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
+            </div>
+            <button style = "border: none; color: white; padding: 12px 1300px;font-size: 30px" class="btn fixed-top">
+                <i class="fa fa-bars"></i>
+            </button>
+            <div class="container justify-content-center">
+                <ul class="navbar-nav fixed-top justify-content-center">
+                    <li class="navbar-brand" style = "font-size: 40px"  href="#">ECE Ebay</li>
+         
+                </ul>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="creer_vendeur.php">Vendeur</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Admin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Creer_acheteur.php">Acheteur</a>
+                    </li>
+                   
+                </ul>
+            </div>
+
+        </nav>
+        
+        <header class="page-header header container-fluid" style = "padding-top: 90px; background-color :#B1B1B1 ">
+            <div class="overlay" style = "padding-top: 90px"></div>
+            <div class="description">
+                <h1>Bienvenue vendeur !</h1>
+   
+            </div>
+        </header> 
+<div class="content" align="center">
+    
         <p>Veuillez entrer vos identifiants pour vous connecter:<p/>
         <div class="center">
             <table>    
@@ -26,16 +91,15 @@ session_start();
 			</tr>
 			<tr>     
 				<td colspan="2" align="center">                                
-					<input type="submit" value="Connection"></td>    
+					<input type = "submit" href = "acceuil_vendeur.php" value="Connection" style = "background-color:#353b48 ; color: #fff; padding : 5px 30px"></td>    
 				</tr>   
 			</table>
 		</div>
-         </form>
-        </div>
-   
-
-
+        
 <?php
+     
+            
+        
 // récupère les données du formulaire
 
 //Connexion à la Base de Données
@@ -46,7 +110,7 @@ session_start();
 
 $Pseudo = isset($_POST["Pseudo"])? $_POST["Pseudo"] : "";  
 $Email = isset($_POST["Email"])? $_POST["Email"] : "";  
-$sql = "SELECT * FROM vendeur";   
+$sql = "SELECT * FROM Vendeur";   
         if ($Pseudo != "") 
         {     
   //on cherche si le vendeur existe
@@ -60,21 +124,45 @@ $sql = "SELECT * FROM vendeur";
       if (mysqli_num_rows($result) != 0) 
         {    
             while ($data = mysqli_fetch_assoc($result)) 
-  {      
-    //recupération des cariables de session
-  $_SESSION['Pseudo']=$Pseudo;
-  $_SESSION['Id_vendeur']=$data['Id_vendeur'];
-  $_SESSION['Email']=$Email;
-  echo "ID: " . $_SESSION['Id_vendeur'] . "<br>";
-}
+            {      
+                    //recupération des cariables de session
+                  $_SESSION['Pseudo']=$Pseudo;
+                  $_SESSION['Id_vendeur']=$data['Id_vendeur'];
+                  $_SESSION['Email']=$Email;
+                  echo "ID: " . $_SESSION['Id_vendeur'] . "<br>";
+            }
+          
+     
+          echo'<a href = "acceuil_vendeur.php">acceuil</a>';
         }    
         else
         {
-          echo "Mauvais identifiant " . "<br>";
+          echo 'Mauvais identifiant   <br>';
         }
+       
 	
 	
 ?>
+    </div>
+         </form>
+        
+   
+
+
+     <header class="page-header header container-fluid"  style ="background-color : #B1B1B1 ">
+            
+            <div class="description" >
+                <h1>Je suis nouveau ici</h1>
+   
+            </div>
+        </header> 
+    
+    <div class="content " style ="background-color : #DBFFE2; height: 500px; padding-top : 150px" align="center">
+        <a href = "creer_vendeur.php"><input value="S'inscrire" style = "background-color:#353b48 ; color: #fff; padding : 7px 35px"></a>
+            
+       
+        
+    </div>
     
     </body>
     </html>
