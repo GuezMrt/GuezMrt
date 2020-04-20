07aa68de10e3e61f5e92eec0ea2f18daf6ae2423
gauthierbcn <49686586+gauthierbcn@users.noreply.github.com>
2020-04-20 12:18:58 +0200

    Update panier.php

--- ProjetWeb/panier.php
+++ ProjetWeb/panier.php
@@ -32,15 +32,55 @@ session_start();
     </head>
     
     <body>
+         <!-- Navbar -->
+        <nav class="navbar navbar-expand-sm fixed-top">
+     
+            <div class="searchbar fixed-top">
+            <input class="search_input" type="text" name="" placeholder="Rechercher...">
+                <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
+            </div>
+            <button style = "border: none; color: white; padding: 12px 1300px;font-size: 30px" class="btn fixed-top">
+                <i class="fa fa-bars"></i>
+            </button>
+            <div class="container justify-content-center">
+                <ul class="navbar-nav fixed-top justify-content-center">
+                    <li class="navbar-brand" style = "font-size: 40px"  href="accueil.php">ECE Ebay</li>
+         
+                </ul>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="articles_vendeur.php">Articles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="articles_ajouter.php">Ajouter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="messages.php">Messages</a>
+                    </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="Compte.php">Compte</a>
+                    </li>
+                </ul>
+            </div>
+
+        </nav>
+            <header class="page-header header container-fluid" style = "padding-top: 90px; background-color :#B1B1B1 ">
+            <div class="overlay" style = "padding-top: 90px"></div>
+            <div class="description">
+                <h1>Panier de toi !</h1>
+   
+            </div>
+        </header> 
 <?php  
+        
   //selection bdd
  $database = "piscine";    
 //connection Bdd
  $db_handle = mysqli_connect('localhost', 'root', 'root');  
  $db_found = mysqli_select_db($db_handle, $database); 
-  $Id_item = $_GET['id'];
-
-      //echo'item : '. $Id_item .'<br>';
+  
+$Id_item = $_GET['id'];
+      echo'item : '. $Id_item .'<br>';
       //recupération de l'id panier en fonction de l'acheteur connecté
       $Id_acheteur = $_SESSION['Id_acheteur'];
       echo'acheteur : '. $Id_acheteur .'<br>';
@@ -64,6 +104,8 @@ session_start();
       $sql = "INSERT INTO Contient(Id_panier, Id_item)VALUES('$Id_panier', '$Id_item')";
       $result = mysqli_query($db_handle, $sql); 
       
+      $sql = "SELECT * FROM Contient";
+        $result = mysqli_query($db_handle, $sql);
         while ($data = mysqli_fetch_assoc($result)) 
           {      
               //echo'panier '. $data['Id_panier'] .'<br>';
@@ -88,4 +130,3 @@ mysqli_close($db_handle);
   ?>
     </body>
 </html>
-
